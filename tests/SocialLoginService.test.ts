6593dab2a0f3b9c6b23ec964c8107771e1400dfd
happyeveryone96 <happyeveryone@naver.com>
2023-03-20 17:40:23 +0900

    test: 소셜 로그인 서비스 관련 테스트 코드 추가

--- tests/SocialLoginService.test.ts
+++ tests/SocialLoginService.test.ts
@@ -1,6 +1,40 @@
 import SocialLoginService from '../src/services/SocialLoginService';
 
 describe('SocialLoginService', () => {
-  it.skip('should login through kakao', async () => {});
-  it.skip('should login through naver', () => {});
+  it('should login through kakao', async () => {
+    const service = new SocialLoginService({ provider: 'kakao' });
+    const resp = await service.login();
+
+    const expectedResponse = Promise.resolve({
+      provider: {
+        name: 'kakao',
+        id: 'be35f10d-1214-46af-af06-916429eaf77d',
+      },
+      profile: {
+        userId: 'be35f10d-1214-46af-af06-916429eaf77d',
+        email: 'test@example.com',
+        scopes: ['email', 'name'],
+      },
+    });
+
+    expect(Promise.resolve(resp)).toStrictEqual(expectedResponse);
+  });
+  it('should login through naver', async () => {
+    const service = new SocialLoginService({ provider: 'naver' });
+    const resp = await service.login();
+
+    const expectedResponse = Promise.resolve({
+      provider: {
+        name: 'naver',
+        id: 'be35f10d-1214-46af-af06-916429eaf77d',
+      },
+      profile: {
+        username: 'test',
+        email: 'test@example.com',
+        scopes: ['email', 'name'],
+      },
+    });
+
+    expect(Promise.resolve(resp)).toStrictEqual(expectedResponse);
+  });
 });
